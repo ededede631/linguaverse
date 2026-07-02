fcb5ac71a5874692caa499340d7aa2f64079b53b
ededede631 <3931710611@qq.com>
2026-07-02 12:24:21 +0800

    Update sw.js for LinguaVerse fixes

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "linguaverse-static-cache-20260703b";
+const CACHE_NAME = "linguaverse-static-cache-20260703c";
 const CORE_ASSETS = [
   "/linguaverse/",
   "/linguaverse/index.html",
